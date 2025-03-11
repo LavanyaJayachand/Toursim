e101b6f338b3135fa4c6a3986f70adfb9db382f6
Lavanya J <lavanyajayachandran.vjc@gmail.com>
2025-03-11 15:38:36 +0530

    updated navigation

--- sojourn.js
+++ sojourn.js
@@ -13,29 +13,28 @@ function Toggle(className) {
     }
 }
 
-function switchh(event){
+function switchh(event) {
 
     event.preventDefault()
-    let s1=document.getElementById("wa");
-    let s2=document.getElementById("yo");
-    let s3=document.getElementById("ws") ;
+    let s1 = document.getElementById("wa");
+    let s2 = document.getElementById("yo");
+    let s3 = document.getElementById("ws");
 
-    let input=document.querySelector("input.in")
-    if(input.value=="whatsapp"){
-        window.location.href="https://web.whatsapp.com/"
+    let input = document.querySelector("input.in")
+    if (input.value == "whatsapp") {
+        window.location.href = "https://web.whatsapp.com/"
     }
-    else if(input.value=="youtube")
-    {
-        window.location.href="https://www.youtube.com/"
+    else if (input.value == "youtube") {
+        window.location.href = "https://www.youtube.com/"
     }
-    else if(input.value=="w3schools"){
-        window.location.href="https://www.w3schools.com/"
+    else if (input.value == "w3schools") {
+        window.location.href = "https://www.w3schools.com/"
     }
 
 }
 
-function navigateToShowcase() {
-    // Navigate to page2.html and pass a query parameter for the target section (showcase)
-    window.location.href = 'ThemeExtension.html?main-c4=true';
 
+function navigateTo(sectionId) {
+    window.location.href = `ThemeExtension.html#${sectionId}`;
 }
+
